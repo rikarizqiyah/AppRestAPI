62b8d30a1e7e927b21e153afa2905fba2c6c0109
rikarizqiyah <rikaraudhotul@gmail.com>
2023-03-04 08:56:48 +0700

    Berhasil ubah data

--- controller.js
+++ controller.js
@@ -48,3 +48,21 @@ exports.tambahMahasiswa = function (req, res) {
         }
     );
 };
+
+// Mengubah data berdasarkan id
+exports.ubahMahasiswa = function(req,res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE Mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim,nama,jurusan,id],
+        function(error, rows, fields) {
+            if(error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil ubah data", res)
+            }
+        }
+    );
+}
