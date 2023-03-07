c00e0076e94776b683a96a16199f5198afd72020
rikarizqiyah <rikaraudhotul@gmail.com>
2023-03-07 13:08:22 +0700

    Registrasi berhasil

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 // Parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Panggil routes
 var routes = require('./routes');
 routes(app);
+
+// Daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
